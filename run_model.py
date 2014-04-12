cafe70bd1ebcebcede6df5ac31c5bc2514d61b66
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-04-12 22:19:05 +0200

    run_model

---
+++ run_model.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+from CONFIG import path_liam, path_til
+import sys 
+
+sys.path.append(path_liam)
+from src.simulation import Simulation
+
+fichier = path_til + 'Model\\console_futur.yml'
+path_model = 'C:\\Til-BaseModel\\console.yml'
+
+
+simulation = Simulation.from_yaml(path_model,
+                    input_dir = None,
+                    input_file = None,
+                    output_dir = path_til + 'output',                    
+                    output_file = None)
+simulation.run(False)
+
+# import cProfile
+# command = """simulation.run(False)"""
+# cProfile.runctx( command, globals(), locals(), filename="OpenGLContext.profile1")
