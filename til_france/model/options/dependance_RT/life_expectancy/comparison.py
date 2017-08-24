e6d00077a769a6ebdff93882df91dec214fae7a4
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-08-24 15:41:34 +0200

    Document comparison

--- til_france/model/options/dependance_RT/life_expectancy/comparison.py
+++ til_france/model/options/dependance_RT/life_expectancy/comparison.py
@@ -68,29 +68,36 @@ def get_mortality_from_insee_projection(year = None):
     return mortalite_insee.set_index(['sex', 'age'])
 
 
-def plot_paquid_comparison(formula = None, age_max = 120):
+def plot_paquid_comparison(formula = None, age_max = 120, paquid_years = None):
     """Plot various mortality curves by age  and sex
     mortalite_1988: historical mortality values
-    mortalite_insee_2007: prjected mortality values
-    mortalite
+    mortalite_insee_2007: projected mortality values
+    mortality_from_paquid:
+        mortality computed by applying estimated mortality table to PAQUID data pooled on paquid_years
+    mortality_after_imputation:
+        mortality computed by applying estimated mortality table to TIL data after imputation of disability level
     """
     assert formula is not None
-    transitions = get_transitions_from_formula(formula = formula)
-    mortality_table = get_predicted_mortality_table(transitions = transitions)
-
-    paquid = get_clean_paquid()
-    paquid['final_state'] = paquid.groupby('numero')['initial_state'].shift(-1)
-
-    paquid['sex'] = 'male'
-    paquid.loc[paquid.sexe == 2, 'sex'] = 'female'
-    del paquid['sexe']
-
     mortalite_1988 = extract_historical_mortality(year = 1988)
     mortalite_insee_2007 = get_mortality_from_insee_projection(year = 2007)
 
-    profile = paquid.query('initial_state != 5').dropna()
-    profile['age'] = profile.age.round()
-    profile = (profile
+    transitions = get_transitions_from_formula(formula = formula)
+    mortality_table = get_predicted_mortality_table(transitions = transitions)
+
+    # Applying mortality table to PAQUID data
+    paquid = get_clean_paquid()
+
+    paquid['final_state'] = paquid.groupby('numero')['initial_state'].shift(-1)
+    paquid['sex'] = 'male'
+    paquid.loc[paquid.sexe == 2, 'sex'] = 'female'
+    del paquid['sexe']
+    paquid = paquid.query('initial_state != 5').dropna()
+    paquid['age'] = paquid.age.round()
+
+    if paquid_years:
+        paquid = paquid.query("annee in @paquid_years")
+
+    mortality_from_paquid = (paquid
         .merge(mortality_table.reset_index(), on =['sex', 'age', 'initial_state'], how = 'left')
         .groupby(['sex', 'age'])['mortality']
         .mean()
@@ -98,16 +105,19 @@ def plot_paquid_comparison(formula = None, age_max = 120):
         .astype(dict(age = 'int'))
         .set_index(['sex', 'age'])
         )
-    profile.name = 'mortality_from_paquid'
 
     period = 2010
     mortality_after_imputation = get_mortality_after_imputation(period = period, mortality_table = mortality_table)
     calibrated_mortality_after_imputation = get_calibrated_mortality_after_imputation(
         transitions = transitions, period = period)
 
+    paquid_mortality_name = 'mortality_from_paquid_{}'.format(
+        ('_').join([str(year) for year in paquid_years]))
+
     plot_data = (pd.concat(
         [
-            profile,
+            mortality_from_paquid.rename(
+                columns = dict(mortality = paquid_mortality_name)),
             mortalite_insee_2007,
             mortalite_1988,
             mortality_after_imputation,
@@ -123,8 +133,7 @@ def plot_paquid_comparison(formula = None, age_max = 120):
         axes[index].set_title("{} mortality".format(index))
         axes[index].get_figure().savefig('mortalite_{}.png'.format(index))
 
-    plot_data['ratio'] = plot_data.eval('mortality / mortalite_insee_2007')
-
+    plot_data['ratio'] = plot_data[paquid_mortality_name] / plot_data.mortalite_insee_2007
     axes2 = plot_data.groupby('sex').plot(x = 'age', y = 'ratio')
 
     for index in axes2.index:
@@ -138,7 +147,7 @@ if __name__ == '__main__':
 
     formula = 'final_state ~ I((age - 80)) + I(((age - 80))**2) + I(((age - 80))**3)'
 
-    plot_data = plot_paquid_comparison(formula = formula, age_max = 95)
+    plot_data = plot_paquid_comparison(formula = formula, age_max = 95, paquid_years = [1988])
 
 #    period = 2010
 #    transitions = get_transitions_from_formula(formula = formula)
