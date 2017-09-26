8b365c0b7e3b2f15bb5a1a526308cf0373ff3694
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2017-09-26 13:45:58 +0200

    Remove wrongly used saves

--- til_france/data/data/handicap_sante_menages.py
+++ til_france/data/data/handicap_sante_menages.py
@@ -46,8 +46,6 @@ def load_dataframe():
 
 def save(dataframe):
     config = Config()
-    hsi_data_directory = config.get('raw_data', 'hsi_data_directory')
-    dataframe.to_hdf(os.path.join(hsi_data_directory, "hsi_extract.h5"), 'individus_institutions')
 
 
 if __name__ == "__main__":
